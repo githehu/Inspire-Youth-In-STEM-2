54eb1a89c7691aa8ed7c26c569e4a7627539970d
githehu <githehudavid888@gmail.com>
2023-02-15 10:56:45 +0300

    list

---
+++ Week-2/for_loop.py
@@ -0,0 +1,14 @@
+sum=0
+for i in range(0,10):
+    sum = sum + i
+    print(sum)
+for k in range(0,100):
+    print(k)
+#print product of first number
+
+product = 1
+for x in range (1,10):
+    product = product * x
+    print(product)
+
+
