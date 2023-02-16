99cee02e7d31db6722d6ccd358581e3370d99fdf
githehu <githehudavid888@gmail.com>
2023-02-16 12:09:56 +0300

    mean.py

---
+++ Week-2/data_types.py
@@ -0,0 +1,22 @@
+#floating vs non floating
+weight = 39.6 #kgs #decimal number
+maths_grade = 72 #non floating
+
+#characters a b c....z
+chem_grade = "D"
+x = 20
+y = 4
+x,y,z = 45,67,86
+
+#arithmetic operations
+z = y + x #addition
+p = x - y #subtraction
+q = x / y #quotient
+m = x % y #remainder #modulus
+
+square_x = x**2 #power
+cube_x = x**3
+
+number = 13
+print("the square of :",number **2)
+print("the cube of the number is:",number **3)
